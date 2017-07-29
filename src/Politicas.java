6b5702539cd4214725f2199822a305f74347a269
Luis Lenta <luislenta@gmail.com>
2017-07-29 19:34:49 -0300

    Reordeno codigo de politicas.Falta segunda opcion de politicas

--- src/Politicas.java
+++ src/Politicas.java
@@ -5,6 +5,10 @@ public class Politicas {
 	private int piezasB;
 	private int piezasC;
 
+	/*
+	 * Los dos procesos tienen la transicion 4, entonces asi, si en las colas
+	 * solo estan estos dos procesos solamente, se despierta esta transicion
+	 */
 	private int proceso1A2[][] = { { 4, 2, 14, 9, 19, 17 } };
 	private int proceso1A1[][] = { { 4, 1, 12, 6, 18, 17 } };
 	private int proceso2[][] = { { 11, 7, 13, 10 } };
@@ -268,6 +272,54 @@ public class Politicas {
 	 * }
 	 */
 
+	/*
+	 * Metodo que buscar si alguna transicion de un proceso se encuentra en la
+	 * matriz m
+	 */
+
+	public boolean tieneTransicion(Matriz proceso, Matriz m) {
+
+		boolean tiene = false;
+		for (int i = 0; i < m.getColCount(); i++) {
+
+			if (m.getValor(0, i) == 1) {
+
+				for (int j = 0; j < proceso.getColCount(); j++) {
+
+					if ((proceso.getValor(0, j) - 1) == i) {
+
+						tiene = true;
+					}
+				}
+			}
+		}
+
+		return tiene;
+	}
+
+	/*
+	 * Busca las transiciones del proceso que coinciden con el vector m y
+	 * devuelve la ultima que encuntra, si no encuentra ninguna devuelve cero.
+	 */
+	public int buscaTransicion(Matriz proceso, Matriz m) {
+		int transicion = 0;
+		for (int i = 0; i < m.getColCount(); i++) {
+
+			if (m.getValor(0, i) == 1) {
+
+				for (int j = 0; j < proceso.getColCount(); j++) {
+
+					if ((proceso.getValor(0, j) - 1) == i) {
+
+						transicion = (int) proceso.getValor(0, j);
+					}
+				}
+			}
+		}
+		System.out.println("El metodo buscaTransicion eligio " + transicion);
+		return transicion;
+	}
+
 	public int cual2(Matriz m) {
 
 		boolean linea1A1 = false;
@@ -277,175 +329,35 @@ public class Politicas {
 
 		int transicion = 0;
 
-		for (int i = 0; i < m.getColCount(); i++) {
+		linea1A1 = this.tieneTransicion(process1A1, m);
+		linea1A2 = this.tieneTransicion(process1A2, m);
+		linea2 = this.tieneTransicion(process2, m);
+		linea3 = this.tieneTransicion(process3, m);
 
-			if (m.getValor(0, i) == 1) {
-				
-				System.out.println("m tiene un 1 en " + i);
-				for (int j = 0; j < process1A1.getColCount(); j++) { // Busco si
-																		// alguna
-																		// transicion
-																		// de m
-																		// pertenece
-																		// a la
-																		// linea
-																		// de
-																		// produccion
-																		// 1
-																		// alternativa
-																		// 1
-
-					if ((process1A1.getValor(0, j) - 1) == i) {
-							
-						linea1A1 = true;
-						
-					}
-				}
-				for (int j = 0; j < process1A2.getColCount(); j++) { // Busco si
-																		// alguna
-																		// transicion
-																		// de m
-																		// pertenece
-																		// a la
-																		// linea
-																		// de
-																		// produccion
-																		// 1
-																		// alternativa
-																		// 2
-
-					if ((process1A2.getValor(0, j) - 1) == i) {
-
-						linea1A2 = true;
-					}
-				}
-				for (int j = 0; j < process2.getColCount(); j++) { // Busco si
-																	// alguna
-																	// transicion
-																	// de m
-																	// pertenece
-																	// a la
-																	// linea de
-																	// produccion
-																	// 2
-					System.out.println("valor de process2 " + (process2.getValor(0, j) - 1));
-					if ((process2.getValor(0, j) - 1) == i) {
-						
-						
-						linea2 = true;
-						System.out.println("cambio linea2" + linea2);
-					}
-				}
-				for (int j = 0; j < process3.getColCount(); j++) { // Busco si
-																	// alguna
-																	// transicion
-																	// de m
-																	// pertenece
-																	// a la
-																	// linea de
-																	// produccion
-																	// 3
-
-					if ((process3.getValor(0, j) - 1) == i) {
-
-						linea3 = true;
-					}
-				}
-			}
-		}
-		
-		System.out.println("Las lineas estan: L1A1= " +linea1A1 + " L1A2= " + linea1A2 + " L2= " + linea2 + " L3= " + linea3  );
+		System.out.println("Las lineas estan: L1A1= " + linea1A1 + " L1A2= " + linea1A2 + " L2= " + linea2 + " L3= " + linea3);
 
 		if (linea1A1 && linea1A2 && linea2 && linea3) {
 
 			if (this.getPorcentajePiezaB() < 50) {
 
-				System.out.println("Es menor el porcentaje de B a 50");
-
-				for (int i = 0; i < m.getColCount(); i++) { // recorro la matriz
-															// m
-
-					if (m.getValor(0, i) == 1) { // verifico que transiciones
-													// estan en la matriz m
-
-						for (int j = 0; j < process2.getColCount(); j++) { // recorro
-																			// la
-																			// matriz
-																			// del
-																			// proceso
-
-							if (i == process2.getValor(0, j) - 1) { // si la
-																	// transicion
-																	// que tenia
-																	// un
-																	// 1 en m
-																	// coincide
-																	// con
-																	// una
-																	// transicion
-																	// del
-																	// proceso,
-																	// entonces
-																	// puedo
-																	// despertar
-																	// ese
-																	// hilo.
-																	// Resto en
-																	// uno
-																	// porque i
-																	// arranca
-																	// de 0
-								transicion = (int) process2.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
-			}
-
-			else {
+				System.out.println("Es menor el porcentaje de B a 50, jajajajajajaaaaaaaaaaaaaaaaa");
+				transicion = this.buscaTransicion(process2, m);
+			} else {
 
 				System.out.println("Es mayor el porcentaje de B a 50, pieza A");
-				if (this.getPiezasA() <= this.getPiezasC()) { // Si hay menos
-																// piezas
-																// A que piezas
-																// C,
-																// busco hilos
-																// dentro de m
-																// que
-																// sean del
-																// proceso
-																// de A
+				if (this.getPiezasA() <= this.getPiezasC()) {
 
-					for (int i = 0; i < m.getColCount(); i++) {
+					/*
+					 * Busco en los dos procesos, total los dos tienen la
+					 * transicion 4. Y eso me permite que si no hay otra
+					 * transicion sensibilizada mas que la 4, en el process1A2,
+					 * no me genera ningun error, porque queda transicion = 4, y
+					 * la 4 estoy seguro que esta sensibilizada
+					 */
 
-						if (m.getValor(0, i) == 1) {
+					transicion = this.buscaTransicion(process1A1, m);
+					transicion = this.buscaTransicion(process1A2, m);
 
-							for (int j = 0; j < process1A1.getColCount(); j++) {
-
-								if (i == process1A1.getValor(0, j) - 1) {
-									transicion = (int) process1A1.getValor(0, j);
-									System.out.println("Elegi la transicion " + transicion);
-								}
-							}
-
-						}
-					}
-
-					for (int i = 0; i < m.getColCount(); i++) {
-
-						if (m.getValor(0, i) == 1) {
-
-							for (int j = 0; j < process1A2.getColCount(); j++) {
-
-								if (i == process1A2.getValor(0, j) - 1) {
-									transicion = (int) process1A2.getValor(0, j);
-									System.out.println("Elegi la transicion " + transicion);
-								}
-							}
-
-						}
-					}
 				} else { // Si hay menos
 							// piezas C
 							// que
@@ -459,53 +371,17 @@ public class Politicas {
 							// de C
 
 					System.out.println("Es mayor el porcentaje de B a 50, pieza C");
-					for (int i = 0; i < m.getColCount(); i++) {
-						if (m.getValor(0, i) == 1) {
 
-							for (int j = 0; j < process3.getColCount(); j++) {
-
-								if (i == process3.getValor(0, j) - 1) {
-									transicion = (int) process3.getValor(0, j);
-									System.out.println("Elegi la transicion " + transicion);
-								}
-							}
-						}
-					}
+					transicion = this.buscaTransicion(process3, m);
 				}
 			}
 		}
 
 		else if (linea1A1 && linea1A2) {
 
-			for (int i = 0; i < m.getColCount(); i++) {
+			transicion = this.buscaTransicion(process1A1, m);
 
-				if (m.getValor(0, i) == 1) {
-
-					for (int j = 0; j < process1A1.getColCount(); j++) {
-
-						if (i == process1A1.getValor(0, j) - 1) {
-							transicion = (int) process1A1.getValor(0, j);
-							System.out.println("Elegi la transicion " + transicion);
-						}
-					}
-
-				}
-			}
-
-			for (int i = 0; i < m.getColCount(); i++) {
-
-				if (m.getValor(0, i) == 1) {
-
-					for (int j = 0; j < process1A2.getColCount(); j++) {
-
-						if (i == process1A2.getValor(0, j) - 1) {
-							transicion = (int) process1A2.getValor(0, j);
-							System.out.println("Elegi la transicion " + transicion);
-						}
-					}
-
-				}
-			}
+			transicion = this.buscaTransicion(process1A2, m);
 		}
 
 		else if (linea1A1 && linea2) {
@@ -514,74 +390,25 @@ public class Politicas {
 
 				System.out.println("Es menor el porcentaje de B a 50");
 
-				for (int i = 0; i < m.getColCount(); i++) { // recorro la matriz
-															// m
-
-					if (m.getValor(0, i) == 1) { // verifico que transiciones
-													// estan en la matriz m
-
-						for (int j = 0; j < process2.getColCount(); j++) { // recorro
-																			// la
-																			// matriz
-																			// del
-																			// proceso
-
-							if (i == process2.getValor(0, j) - 1) {
-								transicion = (int) process2.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
+				transicion = this.buscaTransicion(process2, m);
+				
 			} else {
-				for (int i = 0; i < m.getColCount(); i++) {
 
-					if (m.getValor(0, i) == 1) {
+				transicion = this.buscaTransicion(process1A1, m);
 
-						for (int j = 0; j < process1A1.getColCount(); j++) {
-
-							if (i == process1A1.getValor(0, j) - 1) {
-								transicion = (int) process1A1.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-
-					}
-				}
 			}
 
 		} else if (linea1A1 && linea3) {
 
 			if (this.getPiezasA() < this.getPiezasC()) {
-				for (int i = 0; i < m.getColCount(); i++) {
 
-					if (m.getValor(0, i) == 1) {
-
-						for (int j = 0; j < process1A1.getColCount(); j++) {
-
-							if (i == process1A1.getValor(0, j) - 1) {
-								transicion = (int) process1A1.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-
-					}
-				}
+				transicion = this.buscaTransicion(process1A1, m);
+				
 			} else {
 
 				System.out.println("Es mayor el porcentaje de B a 50, pieza C");
-				for (int i = 0; i < m.getColCount(); i++) {
-					if (m.getValor(0, i) == 1) {
+				transicion = this.buscaTransicion(process3, m);
 
-						for (int j = 0; j < process3.getColCount(); j++) {
-
-							if (i == process3.getValor(0, j) - 1) {
-								transicion = (int) process3.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
 			}
 		} else if (linea1A2 && linea2) {
 
@@ -589,73 +416,25 @@ public class Politicas {
 
 				System.out.println("Es menor el porcentaje de B a 50");
 
-				for (int i = 0; i < m.getColCount(); i++) { // recorro la matriz
-															// m
+				transicion = this.buscaTransicion(process2, m);
 
-					if (m.getValor(0, i) == 1) { // verifico que transiciones
-													// estan en la matriz m
-
-						for (int j = 0; j < process2.getColCount(); j++) { // recorro
-																			// la
-																			// matriz
-																			// del
-																			// proceso
-
-							if (i == process2.getValor(0, j) - 1) {
-								transicion = (int) process2.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
 			} else {
-				for (int i = 0; i < m.getColCount(); i++) {
 
-					if (m.getValor(0, i) == 1) {
+				transicion = this.buscaTransicion(process1A2, m);
 
-						for (int j = 0; j < process1A2.getColCount(); j++) {
-
-							if (i == process1A2.getValor(0, j) - 1) {
-								transicion = (int) process1A2.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-
-					}
-				}
 			}
 		} else if (linea1A2 && linea3) {
 
 			if (this.getPiezasA() < this.getPiezasC()) {
-				for (int i = 0; i < m.getColCount(); i++) {
 
-					if (m.getValor(0, i) == 1) {
-
-						for (int j = 0; j < process1A2.getColCount(); j++) {
-
-							if (i == process1A2.getValor(0, j) - 1) {
-								transicion = (int) process1A2.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-
-					}
-				}
+				transicion = this.buscaTransicion(process1A2, m);
+				
 			} else {
 
 				System.out.println("Es mayor el porcentaje de B a 50, pieza C");
-				for (int i = 0; i < m.getColCount(); i++) {
-					if (m.getValor(0, i) == 1) {
 
-						for (int j = 0; j < process3.getColCount(); j++) {
-
-							if (i == process3.getValor(0, j) - 1) {
-								transicion = (int) process3.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
+				transicion = this.buscaTransicion(process3, m);
+				
 			}
 		} else if (linea2 && linea3) {
 
@@ -663,115 +442,36 @@ public class Politicas {
 
 				System.out.println("Es menor el porcentaje de B a 50");
 
-				for (int i = 0; i < m.getColCount(); i++) { // recorro la matriz
-															// m
+				transicion = this.buscaTransicion(process2, m);
 
-					if (m.getValor(0, i) == 1) { // verifico que transiciones
-													// estan en la matriz m
-
-						for (int j = 0; j < process2.getColCount(); j++) { // recorro
-																			// la
-																			// matriz
-																			// del
-																			// proceso
-
-							if (i == process2.getValor(0, j) - 1) {
-								transicion = (int) process2.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
 			} else {
 
 				System.out.println("Es mayor el porcentaje de B a 50, pieza C");
-				for (int i = 0; i < m.getColCount(); i++) {
-					if (m.getValor(0, i) == 1) {
 
-						for (int j = 0; j < process3.getColCount(); j++) {
-
-							if (i == process3.getValor(0, j) - 1) {
-								transicion = (int) process3.getValor(0, j);
-								System.out.println("Elegi la transicion " + transicion);
-							}
-						}
-					}
-				}
+				transicion = this.buscaTransicion(process3, m);
+				
 			}
-		}
-		else if(linea1A1){
-			for (int i = 0; i < m.getColCount(); i++) {
+		} else if (linea1A1) {
 
-				if (m.getValor(0, i) == 1) {
+			transicion = this.buscaTransicion(process1A1, m);
 
-					for (int j = 0; j < process1A1.getColCount(); j++) {
+		} else if (linea1A2) {
 
-						if (i == process1A1.getValor(0, j) - 1) {
-							transicion = (int) process1A1.getValor(0, j);
-							System.out.println("Elegi la transicion " + transicion);
-						}
-					}
+			transicion = this.buscaTransicion(process1A2, m);
 
-				}
-			}
-		}
-		else if(linea1A2){
-			for (int i = 0; i < m.getColCount(); i++) {
-
-				if (m.getValor(0, i) == 1) {
-
-					for (int j = 0; j < process1A2.getColCount(); j++) {
-
-						if (i == process1A2.getValor(0, j) - 1) {
-							transicion = (int) process1A2.getValor(0, j);
-							System.out.println("Elegi la transicion " + transicion);
-						}
-					}
-
-				}
-			}
-		}
-		else if(linea2){
+		} else if (linea2) {
 			System.out.println("Es menor el porcentaje de B a 50");
 
-			for (int i = 0; i < m.getColCount(); i++) { // recorro la matriz
-														// m
+			transicion = this.buscaTransicion(process2, m);
+		
+		} else if (linea3) {
 
-				if (m.getValor(0, i) == 1) { // verifico que transiciones
-												// estan en la matriz m
-
-					for (int j = 0; j < process2.getColCount(); j++) { // recorro
-																		// la
-																		// matriz
-																		// del
-																		// proceso
-
-						if (i == process2.getValor(0, j) - 1) {
-							transicion = (int) process2.getValor(0, j);
-							System.out.println("Elegi la transicion " + transicion);
-						}
-					}
-				}
-			}
-		}
-		else if(linea3){
-			
 			System.out.println("Es mayor el porcentaje de B a 50, pieza C");
-			for (int i = 0; i < m.getColCount(); i++) {
-				if (m.getValor(0, i) == 1) {
 
-					for (int j = 0; j < process3.getColCount(); j++) {
+			transicion = this.buscaTransicion(process3, m);
 
-						if (i == process3.getValor(0, j) - 1) {
-							transicion = (int) process3.getValor(0, j);
-							System.out.println("Elegi la transicion " + transicion);
-						}
-					}
-				}
-			}
-		}
-		else{
-			
+		} else {
+
 			System.out.println("Error grande como una casa");
 		}
 		return transicion;
